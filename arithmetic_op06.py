f57f444df84afb645baf422c25511e713240348f
PRESTIGE <mamatqulov@gmail.com>
2022-12-24 19:09:02 +0500

    06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,5 +1,5 @@
 # Create a variable called 'number' and assign any value of int to it.
-number = 67
+number = int(67)
 # Divide the number by 2 and assign number.
 num = number / 2
 # Find the fraction of the number and assign to a variable called 'answer'.
